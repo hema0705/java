7f9b9db0de3312abfd6a493c003b89b02f6c525d
hema0705 <informtohema7@gmail.com>
2021-07-22 14:45:16 +0530

    firstupdate

--- src/org/test/Continue.java
+++ src/org/test/Continue.java
@@ -1,6 +1,12 @@
 package org.test;
 
-public class Continue {public static void main(String[] args) {
+public class Continue {
+	private void cost() {
+System.out.println("1000");
+	}	
+	public static void main(String[] args) {
+		Continue x = new Continue();
+		x.cost();	
     for (int i = 1; i <= 10; i++) {
         if (i == 5) {
           continue;
