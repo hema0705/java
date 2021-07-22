63bbce502cc6b36708fb40685778d9b98ed6fd5d
hema0705 <informtohema7@gmail.com>
2021-07-22 13:30:24 +0530

    kavi

---
+++ src/org/test/Program8.java
@@ -0,0 +1,16 @@
+
+package org.test;
+
+public class Program8 {
+	public static void main(String[] args) {
+		{
+			for(int i=1;i<=3;i++)
+			{
+				for (int j = i+1; j <=i; j++)
+				{
+					System.out.println(j);
+				}
+			}
+		}
+	}
+}
